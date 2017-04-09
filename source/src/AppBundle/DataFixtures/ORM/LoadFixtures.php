2f1e63160f563a58e9da8e5168b6a1176874c7c6
Eugene <craizyeugene@gmail.com>
2017-04-09 13:06:01 +0300

    Custom genus formatter

--- source/src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ source/src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -8,8 +8,47 @@ use Nelmio\Alice\Fixtures;
 
 class LoadFixtures implements FixtureInterface
 {
+    /**
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
-        Fixtures::load(__DIR__.'/fixtures.yml', $manager);
+        Fixtures::load(
+            __DIR__.'/fixtures.yml',
+            $manager,
+            [
+                // Additional objects that provide us with formatter functions
+                'providers' => [$this]
+            ]
+        );
+    }
+
+
+    /**
+     * Genus formatter (returns one random genera on each call)
+     * @return string
+     */
+    public function genus()
+    {
+        $genera = [
+            'Octopus',
+            'Balaena',
+            'Orcinus',
+            'Hippocampus',
+            'Asterias',
+            'Amphiprion',
+            'Carcharodon',
+            'Aurelia',
+            'Cucumaria',
+            'Balistoides',
+            'Paralithodes',
+            'Chelonia',
+            'Trichechus',
+            'Eumetopias'
+        ];
+
+        $key = array_rand($genera);
+
+        return $genera[$key];
     }
 }
