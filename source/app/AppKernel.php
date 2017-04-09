1a23360f3c8e466eaa2e5325ba656c69e57c6cdc
Eugene <craizyeugene@gmail.com>
2017-04-09 12:32:04 +0300

    DoctrineFixturesBundle installed

--- source/app/AppKernel.php
+++ source/app/AppKernel.php
@@ -21,11 +21,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
+        // Available only in 'dev' and 'test' environments
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
